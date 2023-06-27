cd94521ce1ed640aab0afdbc5ba3f19e619f1f39
gluoneros <gluoneros@gmail.com>
2023-06-27 12:59:57 -0500

    configuracion taild js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        corben: ['Corben', 'cursive'],
+        display: ['Playfair Display', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
-
